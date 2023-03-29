9f4715abf8547af2a7a2bb622aef5038e5398cb3
Chik Him <tang.chikhim@gmail.com>
2023-03-29 22:30:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,9 +26,10 @@ st.write("For current rank of teams, you may refer to https://www.premierleague.
 
 #Text inputs 
 st.write("")
-st.write("")
-home_name = st.text_input('Home Team Name')
-away_name = st.text_input('Away Team Name')
+home_name = st.selectbox('',['Arsenal','Aston Villa','Bournemouth','Brentford','Brighton & Hove Albion','Chelsea','Crystal Palace','Everton','Fulham','Leeds United','Leicester City','Liverpool','Manchester City','Manchester United','Newcastle United','Nottingham Forest','Southampton','Tottenham Hotspur','West Ham United','Wolverhampton Wanderers'])
+
+away_name = st.selectbox('',['Arsenal','Aston Villa','Bournemouth','Brentford','Brighton & Hove Albion','Chelsea','Crystal Palace','Everton','Fulham','Leeds United','Leicester City','Liverpool','Manchester City','Manchester United','Newcastle United','Nottingham Forest','Southampton','Tottenham Hotspur','West Ham United','Wolverhampton Wanderers']) 
+
 Bet_Amount = st.sidebar.number_input('Bet Amount', 1.0, 100000000.0, 10.0)
 
 
@@ -196,3 +197,8 @@ st.write("Disclaimer: Please note that this information should not be construed 
 # st.subheader('Potential Winnings:')
 # st.metric('', f'${winnings}', '')
 
+
+
+# st.write("")
+# home_name = st.text_input('Home Team Name')
+# away_name = st.text_input('Away Team Name')
